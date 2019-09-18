9b6c9a88888eca8f512cb8c42db1338f13fcc884
Jchaochao <1500012718@pku.edu.cn>
2019-09-18 00:37:36 -0400

    Add comments on 'Longest Substring Without Repeating Characters'

--- Longest Substring Without Repeating Characters.cpp
+++ Longest Substring Without Repeating Characters.cpp
@@ -1,4 +1,12 @@
-//
+/*
+Like a sliding window, we define S(n) is the
+longest substring number in the first n characters,
+Then we can get S(n) = S(n-1)  when last character in
+the sequence plus the S(n-1) characters before have
+repeat, and S(n) = S(n-1) + 1,if we find the
+last number and the S(n-1) numbers is a not
+repeated sequence.
+*/
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
