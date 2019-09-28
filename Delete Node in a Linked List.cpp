3cd7ee2b61f8d74247a25269a6983fbe73ff4ed2
Jchaochao <1500012718@pku.edu.cn>
2019-09-28 17:15:47 -0400

    Solution of 'Delete Node in a Linked List'

---
+++ Delete Node in a Linked List.cpp
@@ -0,0 +1,20 @@
+/*
+The key is to understand the proble, all you need to do is to delete the given node.
+The problem is why we need to copy the val and next rather then be the next.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
