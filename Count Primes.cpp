b97854cff9f4c37d21fdf2e913bb5858c3d109e1
Jchaochao <1500012718@pku.edu.cn>
2019-10-11 14:12:23 -0400

    Add another solution.

--- Count Primes.cpp
+++ Count Primes.cpp
@@ -23,3 +23,28 @@ public:
         return count;
     }
 };
+
+/*
+Make a n-lenght vector record to record multiply of some small
+numbers as 1
+*/
+class Solution {
+public:
+    int countPrimes(int n) {
+        if(n<=2)
+            return 0;
+        int count =1;
+        vector<bool> record(n,false);
+        for(int i = 3;i < n ; i+=2)
+        {
+            if(!record[i])
+            {
+                count++;
+                for(int j = 3;i*j<n;j+=2)
+                    record[i*j] = 1;
+
+            }
+        }
+        return count;
+    }
+};
