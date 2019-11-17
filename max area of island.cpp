b1b3a48c0f3e7bfb284cb0694f79c5e12a7f18ca
Jchaochao <1500012718@pku.edu.cn>
2019-11-17 14:52:14 -0500

    Create max area of island.cpp

---
+++ max area of island.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int n = grid.size();
+        if(n==0)
+            return 0;
+        int m = grid[0].size();
+        int result = 0;
+        int temp = 0;
+        for(int i = 0;i<n;++i)
+        {
+            for(int j = 0;j<m;++j)
+                if(grid[i][j]==1)
+                {
+                    temp = 0;
+                    getArea(grid,i,j,temp);
+                    if(temp>result)
+                        result = temp;
+                }
+        }
+        return result;
+    }
+private:
+    void getArea(vector<vector<int>>& grid, int i ,int j ,int& temp )
+    {
+        temp+=1;
+        grid[i][j]=-1;
+        int n = grid.size();
+        int m = grid[0].size();
+        if(i>0 && grid[i-1][j]==1)
+            getArea(grid,i-1,j,temp);
+        if(j>0 && grid[i][j-1]==1)
+            getArea(grid,i,j-1,temp);
+        if(i<n-1 && grid[i+1][j]==1)
+            getArea(grid,i+1,j,temp);
+        if(j<m-1 && grid[i][j+1]==1)
+            getArea(grid,i,j+1,temp);
+        return;
+    }
+};
